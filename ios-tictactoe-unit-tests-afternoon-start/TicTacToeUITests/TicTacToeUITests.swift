4aacf5273129e27c40d735c56fe153132bd00f24
Brandi <blubrandi0@gmail.com>
2019-12-12 11:59:07 -0700

    Added UI Tests and sample async test

--- ios-tictactoe-unit-tests-afternoon-start/TicTacToeUITests/TicTacToeUITests.swift
+++ ios-tictactoe-unit-tests-afternoon-start/TicTacToeUITests/TicTacToeUITests.swift
@@ -20,4 +20,98 @@ class TicTacToeUITests: XCTestCase {
         
         XCUIApplication().launch()
     }
+    
+    func testWinCheckingVertical1() {
+        /*
+         0 1 2
+         3 4 5
+         6 7 8
+         */
+        
+        /*
+         x o -
+         x o -
+         x - -
+         */
+        
+        
+        let button0 = button(forIndex: 0)
+        let button1 = button(forIndex: 1)
+        let button3 = button(forIndex: 3)
+        let button4 = button(forIndex: 4)
+        let button6 = button(forIndex: 6)
+        
+        button0.tap()
+        XCTAssertEqual(button0.label, "X")
+        XCTAssertEqual(turnLabel.label, "Player O's turn")
+        
+        button1.tap()
+        XCTAssertEqual(button1.label, "O")
+        XCTAssertEqual(turnLabel.label, "Player X's turn")
+        
+        button3.tap()
+        XCTAssertEqual(button3.label, "X")
+        XCTAssertEqual(turnLabel.label, "Player O's turn")
+        
+        button4.tap()
+        XCTAssertEqual(button4.label, "O")
+        XCTAssertEqual(turnLabel.label, "Player X's turn")
+        
+        button6.tap()
+        XCTAssertEqual(button6.label, "X")
+        XCTAssertEqual(turnLabel.label, "Player X won!")
+        
+    }
+    
+    func testRestartingGame() {
+        // Tap someplace in the board so it isn't empty
+        let button4 = button(forIndex: 4)
+        button4.tap()
+        
+        // Make sure the board has been marked
+        XCTAssertEqual(button4.label, "X")
+        
+        // Check the next player's turn label
+        XCTAssertEqual(turnLabel.label, "Player O's turn")
+        
+        // Make sure the board is empty again
+        restartButton.tap()
+        
+        for i in 0...8 {
+            XCTAssertEqual(button(forIndex: i).label, " ")
+        }
+    }
+    
+    /////////////
+    // Test Example of async ui code
+    /////////////
+    
+    func testDarkSideCharacters() {
+        
+        let fetchedCharactersExpectation = expectation(for: NSPredicate(format: "count > 0"), evaluatedWith: app.tables.cells, handler: nil)
+        
+        fetchedCharactersExpectation.expectationDescription = "Characters have been fetched and are displayed."
+        
+        app.buttons["fetchDarkSideButton"].tap()
+        
+        waitForExpectations(timeout: 5)
+    }
+    
+    //MARK: Private
+    
+    private var restartButton: XCUIElement {
+        return app.buttons["GameViewController.RestartButton"]
+    }
+    
+    private var app: XCUIApplication {
+        return XCUIApplication()
+    }
+    
+    private var turnLabel: XCUIElement {
+        return XCUIApplication().staticTexts["GameViewController.TurnLabel"]
+    }
+    
+    private func button(forIndex index: Int) -> XCUIElement {
+        return XCUIApplication().buttons["button\(index)"]
+    }
 }
